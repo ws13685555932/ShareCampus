08d74fe7de6e2d09cf7f437581bc9fcfac987c54
wangsheng <1748532294@qq.com>
2017-09-30 17:49:32 +0800

    任务详情界面基本完成

---
+++ app/src/main/java/com/wangsheng/sharecampus/activity/TaskActivity.java
@@ -0,0 +1,15 @@
+package com.wangsheng.sharecampus.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wangsheng.sharecampus.R;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
