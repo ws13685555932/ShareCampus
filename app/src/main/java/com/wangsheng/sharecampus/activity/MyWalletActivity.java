51d42bdb494bf2992337583baedda41a21632c68
zpzlshcs <1349592419@qq.com>
2017-10-01 14:51:25 +0800

    添加钱包界面

---
+++ app/src/main/java/com/wangsheng/sharecampus/activity/MyWalletActivity.java
@@ -0,0 +1,18 @@
+package com.wangsheng.sharecampus.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wangsheng.sharecampus.R;
+
+/**
+ * Created by windows8 on 2017/10/1.
+ */
+
+public class MyWalletActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_wallet);
+    }
+}
